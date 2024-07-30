71d26303da708694c8cd7d744514c67df34f1087
Zexin <zxuu0075@student.monash.edu>
2024-07-30 22:41:11 +1000

    test

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@ import MyName from './components/MyName.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!123" />
     </div>
 
     <div class="wrapper">
       <MyName msg="Zexin!" />
     </div>
-    
+
   </header>
 
   <main>
